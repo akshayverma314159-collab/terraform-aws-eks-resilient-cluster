dcff449c69845254aad8d17d9f7bce562a5d3167
Akshay Verma <akshayverma314159@gmail.com>
2026-09-10 06:38:56 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+  
+  name = "sre-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 21.0"
+  cluster_name    = "sre-production-cluster"
+  cluster_version = "1.33"
+  
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 5
+      instance_types = ["t3.medium"]
+    }
+  }
+}
